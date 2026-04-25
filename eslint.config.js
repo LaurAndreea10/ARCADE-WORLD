7b208952c13a40857d9dee01f7a3b35e936c564f
LaurAndreea10 <79794410+LaurAndreea10@users.noreply.github.com>
2026-04-25 19:14:10 +0300

    Add browser globals for new helpers

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,7 @@ export default [
         document: 'readonly',
         localStorage: 'readonly',
         URLSearchParams: 'readonly',
+        structuredClone: 'readonly',
       },
     },
     rules: {
